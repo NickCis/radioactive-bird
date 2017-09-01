a8d3ff785576018e296f64a8c7b1179b419d62ce
Nicolas Cisco <ncis20@gmail.com>
2017-09-01 12:46:13 -0300

    Adding connectWithSSR tests

--- src/common/components/connectWithSSR.test.js
+++ src/common/components/connectWithSSR.test.js
@@ -1,4 +1,6 @@
 import connectWithSSR, { getRouteId } from './connectWithSSR';
+import configureMockStore from 'redux-mock-store'
+import { setLoadedInitialData } from '../actions/initialData';
 
 describe('getRouteId', () => {
   it('shoud use `key` as the first option', () => {
@@ -12,5 +14,39 @@ describe('getRouteId', () => {
   it('should throw error it there are no `key` or `path`', () => {
     expect(() => getRouteId({}))
       .toThrow();
+  });
+});
+
+describe('connectWithSSR', () => {
+  const mockStore = configureMockStore();
+
+  it('should get initial data in SSR', () => {
+    let isGetInitialDataCalled = false;
+    const component = () => {};
+    component.getInitialData = () => {
+      isGetInitialDataCalled = true;
+      return Promise.resolve(true);
+    };
+
+    const store = mockStore({ initialData: { pages: [], }, });
+
+    const hocComponent = connectWithSSR()(component);
+
+    hocComponent.getInitialData({
+      dispatch: d => store.dispatch(d),
+      getState: () => store.getState(),
+      route: { key: 'test', },
+    });
+
+    expect(isGetInitialDataCalled).toBeTruthy();
+    expect(store.getActions()).toEqual([setLoadedInitialData('test')]);
+  });
+
+  it('should get initial data in web', () => {
+    // TODO
+  });
+
+  it('should not double fetch', () => {
+    // TODO
   });
 });
