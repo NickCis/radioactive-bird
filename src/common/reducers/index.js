cd0ef2023779bd0f96ba75d5222b48bb1921b8cb
Nicolas Cisco <ncis20@gmail.com>
2017-09-02 21:02:35 -0300

    Added tweets and search reducer to rootReducer

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import initialData from './initialData';
+import tweets from './tweets';
+import search from './search';
 
 const rootReducer = combineReducers({
   // XXX: in order to get req in SSR
   req: s => s || false,
   counter,
   initialData,
+  tweets,
+  search,
 });
 
 export default rootReducer;
