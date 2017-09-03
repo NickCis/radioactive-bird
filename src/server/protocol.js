0b4f9ae471ccc491f61e1cb7726bd215b3851167
Nicolas Cisco <ncis20@gmail.com>
2017-09-02 21:03:16 -0300

    Added server protocol mock implementation

--- src/server/protocol.js
+++ src/server/protocol.js
@@ -1,8 +1,22 @@
+import searchStubResponse from './api/searchStubResponse';
+
 export const fetchCounter = () => {
   return new Promise(rs => {
     console.log('SSR Fetch counter');
     rs({
       number: Math.round(Math.random() * 1000),
     });
+  });
+};
+
+export const searchTweets = query => {
+  return new Promise(rs => {
+    rs(searchStubResponse);
+  });
+};
+
+export const fetchTweet = id => {
+  return new Promise(rs => {
+    rs(searchStubResponse.statuses[0]);
   });
 };
