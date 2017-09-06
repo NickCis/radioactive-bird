e32ab6b944c94cae00f0e5a40bd62d8322910ab7
Nicolas Cisco <ncis20@gmail.com>
2017-09-05 21:33:31 -0300

    Prettier formmating & linting

--- src/common/components/Status.js
+++ src/common/components/Status.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default ({code, children}) => (
-  <Route render={({ staticContext }) => {
-    if (staticContext)
-      staticContext.status = code;
-    return children || null;
-  }} />
+const Status = ({ code, children }) => (
+  <Route
+    render={({ staticContext }) => {
+      if (staticContext) staticContext.status = code;
+      return children || null;
+    }}
+  />
 );
+
+Status.propTypes = {
+  code: PropTypes.number.isRequired,
+  children: PropTypes.node,
+};
+
+export default Status;
--- src/common/components/Title.js
+++ src/common/components/Title.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default ({title, children}) => (
-  <Route render={({ staticContext }) => {
-    if (staticContext)
-      staticContext.title = title;
-    if (document)
-      document.title = title;
-    return children || null;
-  }} />
+const Title = ({ title, children }) => (
+  <Route
+    render={({ staticContext }) => {
+      if (staticContext) staticContext.title = title;
+      if (document) document.title = title;
+      return children || null;
+    }}
+  />
 );
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+export default Title;
